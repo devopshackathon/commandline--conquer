3a6cdf2a989b78a66288f77a321e57223b746f42
xigolle <joey.driessen@live.nl>
2016-03-17 11:26:32 +0100

    add get api for getting rooms list from db add post api for adding rooms to db

--- server.js
+++ server.js
@@ -8,18 +8,80 @@
 var express = require("express");
 var http = require("http");
 var path = require('path');
-
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectId;
+var url = 'mongodb://127.0.0.1:27017/test';
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//app.use(express.json());
+//MongoClient.connect(url, function(err, db) {
+//  assert.equal(null, err);
+//  console.log("Connected correctly to server.");
+//  if (err) {
+//      console.log(err);
+//  }
+//  db.close();
+//});
+
 
 //make sure it gets all the folders and files.
 app.use(express.static(path.join(__dirname, '')));
 
+
+
+
+var findRoom = function (db, callback) {
+    var cursor = db.collection('lokalendb').find(/*Can find specific item by doing "nr":"0.09"*/);
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    })
+}
+
 //get the url and send the index.html
 app.get("/", function (req, res) {
-    
     res.sendFile(path.join(__dirname+"/index.html"));
-    
 });
+app.post("/api/rooms", function (req, res) {
+    var insertRoom = function (db, callback) {
+        db.collection('lokalendb').insertOne({
+            "nr": req.body.nr,
+            "studentenNr": req.body.studentenNr,
+            "tijd": req.body.tijd,
+            "beschikbaar": req.body.beschikbaar,
+            "voorwerpen":
+                {
+                    "hdmi": req.body.hdmi,
+                    "afstandsbediening": req.body.afstandsbediening
+                }
+        }, function (err, result) {
+            assert.equal(err, null);
+            console.log("Inserted a room into the LokaalDB collection.");
+            callback();
+        })
+    }
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        insertRoom(db, function () {
+            db.close();
+        })
+    })
+});
+app.get("/api/rooms", function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        findRoom(db, function () {
+            db.close();
+        })
+    })
+})
+
 //listen to port 3000 
 var serverOptions = {
     port: 3000,
@@ -30,3 +92,4 @@ var server = app.listen(serverOptions, function () {
     var port = server.address().port;
     console.info("Server listening: http://%s:%s", address, port);
 })
+
