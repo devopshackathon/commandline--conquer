8a64dddbb178d5425c8fa7236b57ca6974120abb
Marino Joosens <Marino Joosens>
2016-03-17 11:27:29 +0100

    GET METHODE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 
-app.controller("alles", function ($scope) {
+app.controller("alles", function ($scope,$http) {
     $scope.roomsTaken = ["00.9", "00.10","00.15"];
     $scope.roomsAvailable = ["00.11", "00.12", "00.13", "00.14"];
     $scope.availableStuff = ["afstandsbediening"];
@@ -13,5 +13,20 @@ app.controller("alles", function ($scope) {
         $scope.roomsToReserve.push(room);
         console.log($scope.roomsToReserve);
     }
+
+
+    $http({
+    	method:'GET',
+    	url:'http://localhost:3000/api/rooms'
+    }).succes(function(data)
+    {
+    	console.log("data geladen");
+    	console.log(data);
+    })
+    .error(function(data)
+    {
+    	console.log(error);
+    	})
     //$scope.datetimepicker();
 })
+
