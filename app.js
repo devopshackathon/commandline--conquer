906b1c096db18795a1538fc50df6c53d998914b0
mathiassamyn <mathiassamyn@hotmail.com>
2016-03-17 10:54:41 +0100

    tussenstop 1

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 
 
-app.controller("", function ($scope) {
+app.controller("alles", function ($scope) {
     $scope.roomsTaken = ["00.9", "00.10","00.15"];
     $scope.roomsAvailable = ["00.11", "00.12", "00.13", "00.14"];
     $scope.availableStuff = ["afstandsbediening", "HDMI-kabel"];
 
-    $(function () {
-        $('#datetimepicker1').datetimepicker();
-    });
+    $scope.roomsToReserve = [];
+    $scope.reserveRoom = function(room) {
+        $scope.roomsToReserve.pu
+    }
+    //$scope.datetimepicker();
 })
