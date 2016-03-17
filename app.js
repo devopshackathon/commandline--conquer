3023f6d06b2984bca2d566f3a355e55b7dc4fe12
mathiassamyn <mathiassamyn@hotmail.com>
2016-03-17 12:26:41 +0100

    met aanpassingen

--- app.js
+++ app.js
@@ -14,18 +14,23 @@ app.controller("alles", function ($scope,$http) {
         console.log($scope.roomToReserve);
     }
 
+    $scope.data;
+
     $http({
-    	method:'GET',
-    	url:'http://localhost:3000/api/rooms'
-    }).succes(function(data)
+        method: 'GET',
+        url: "http://datasets.antwerpen.be/v4/gis/wifiopenbaar.json"
+    	//url:'http://localhost:3000/api/rooms'
+    }).success(function(data)
     {
+        $scope.data = data.data;
     	console.log("data geladen");
-    	console.log(data);
+    	console.log(data.data);
+    	console.log(data.data[0].gemeente);
     })
     .error(function(data)
     {
     	console.log(error);
-    	})
+    })
     //$scope.datetimepicker();
 
     $scope.hdmi = false;
@@ -42,18 +47,18 @@ app.controller("alles", function ($scope,$http) {
         } else $scope.afstandsbediening = true;
     }
 
-    $scope.reserve = function () {
-        json =
-        {
-            "nr": $scope.roomToReserve,
-            "studentenNr": $scope.studentenNr,
-            "tijd": "",
-            "beschikbaar": false,
-            "voorwerpen":
-            {
-                "hdmi":  
-            }
-        }
-    }
+    //$scope.reserve = function () {
+    //    json =
+    //    {
+    //        "nr": $scope.roomToReserve,
+    //        "studentenNr": $scope.studentenNr,
+    //        "tijd": "",
+    //        "beschikbaar": false,
+    //        "voorwerpen":
+    //        {
+    //            "hdmi":  
+    //        }
+    //    }
+    //}
     
 })
