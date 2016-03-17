572e52927c8fd114e704eb3d015e730809a3f26c
mathiassamyn <mathiassamyn@hotmail.com>
2016-03-17 10:26:33 +0100

    started angular

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-﻿
+﻿var app = angular.module("myapp", []);
+
+
+
+app.controller("", function ($scope) {
+    $scope.roomsTaken = ["00.9", "00.10","00.15"];
+    $scope.roomsAvailable = ["00.11", "00.12", "00.13", "00.14"];
+    $scope.availableStuff = ["afstandsbediening", "HDMI-kabel"];
+
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+    });
+})
