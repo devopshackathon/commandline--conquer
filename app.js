daed877bb6515d5409ea66263d0027d2df0aa97d
mathiassamyn <mathiassamyn@hotmail.com>
2016-03-17 11:11:39 +0100

    weral ja

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 app.controller("alles", function ($scope) {
     $scope.roomsTaken = ["00.9", "00.10","00.15"];
     $scope.roomsAvailable = ["00.11", "00.12", "00.13", "00.14"];
-    $scope.availableStuff = ["afstandsbediening", "HDMI-kabel"];
+    $scope.availableStuff = ["afstandsbediening"];
+    $scope.takenStuff = ["HDMI-kabel"];
 
     $scope.roomsToReserve = [];
     $scope.reserveRoom = function(room) {
